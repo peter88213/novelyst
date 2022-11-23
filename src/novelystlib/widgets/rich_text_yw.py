c9c6565f089fef8435493369538dfd7f66578c01
peter88213 <peter.88213@t-online.de>
2022-11-23 22:34:10 +0100

    Prepare RichTextYw for dark mode.

--- src/novelystlib/widgets/rich_text_yw.py
+++ src/novelystlib/widgets/rich_text_yw.py
@@ -29,10 +29,18 @@ class RichTextYw(RichTextTk):
         
         Extends the supeclass constructor
         """
-        super().__init__(*args, height=20, width=60, spacing1=10, spacing2=2, wrap='word', padx=10)
+        super().__init__(*args,
+                height=20,
+                width=60,
+                spacing1=10,
+                spacing2=2,
+                wrap='word',
+                padx=10,
+                bg=kwargs['color_text_bg'],
+                fg=kwargs['color_text_fg'],
+                )
         defaultFont = tkFont.nametofont(self.cget('font'))
 
-        em = defaultFont.measure('m')
         defaultSize = defaultFont.cget('size')
         boldFont = tkFont.Font(**defaultFont.configure())
         italicFont = tkFont.Font(**defaultFont.configure())
@@ -42,33 +50,99 @@ class RichTextYw(RichTextTk):
 
         boldFont.configure(weight='bold')
         italicFont.configure(slant='italic')
-        h1Font.configure(size=int(defaultSize * self.H1_SIZE), weight='bold')
-        h2Font.configure(size=int(defaultSize * self.H2_SIZE), weight='bold')
-        h3Font.configure(size=int(defaultSize * self.H3_SIZE), slant='italic')
-
-        self.tag_configure(self.H1_TAG, font=h1Font, spacing3=defaultSize, foreground=kwargs['color_chapter'],
-                           justify='center', spacing1=defaultSize * self.H1_SPACING)
-        self.tag_configure(self.H1_NOTES_TAG, font=h1Font, spacing3=defaultSize, foreground=kwargs['color_notes'],
-                           justify='center', spacing1=defaultSize * self.H1_SPACING)
-        self.tag_configure(self.H1_TODO_TAG, font=h1Font, spacing3=defaultSize, foreground=kwargs['color_todo'],
-                           justify='center', spacing1=defaultSize * self.H1_SPACING)
-        self.tag_configure(self.H1_UNUSED_TAG, font=h1Font, spacing3=defaultSize, foreground=kwargs['color_unused'],
-                           justify='center', spacing1=defaultSize * self.H1_SPACING)
-        self.tag_configure(self.H2_TAG, font=h2Font, spacing3=defaultSize, foreground=kwargs['color_chapter'],
-                           justify='center', spacing1=defaultSize * self.H2_SPACING)
-        self.tag_configure(self.H2_NOTES_TAG, font=h2Font, spacing3=defaultSize, foreground=kwargs['color_notes'],
-                           justify='center', spacing1=defaultSize * self.H2_SPACING)
-        self.tag_configure(self.H2_TODO_TAG, font=h2Font, spacing3=defaultSize, foreground=kwargs['color_todo'],
-                           justify='center', spacing1=defaultSize * self.H2_SPACING)
-        self.tag_configure(self.H2_UNUSED_TAG, font=h2Font, spacing3=defaultSize, foreground=kwargs['color_unused'],
-                           justify='center', spacing1=defaultSize * self.H2_SPACING)
-        self.tag_configure(self.H3_NOTES_TAG, font=h3Font, spacing3=defaultSize, foreground=kwargs['color_notes'],
-                           justify='center', spacing1=defaultSize * self.H3_SPACING)
-        self.tag_configure(self.H3_TODO_TAG, font=h3Font, spacing3=defaultSize, foreground=kwargs['color_todo'],
-                           justify='center', spacing1=defaultSize * self.H3_SPACING)
-        self.tag_configure(self.H3_UNUSED_TAG, font=h3Font, spacing3=defaultSize, foreground=kwargs['color_unused'],
-                           justify='center', spacing1=defaultSize * self.H3_SPACING)
-        self.tag_configure(self.NOTES_TAG, foreground=kwargs['color_notes'])
-        self.tag_configure(self.TODO_TAG, foreground=kwargs['color_todo'])
-        self.tag_configure(self.UNUSED_TAG, foreground=kwargs['color_unused'])
+        h1Font.configure(size=int(defaultSize * self.H1_SIZE),
+                         weight='bold',
+                         )
+        h2Font.configure(size=int(defaultSize * self.H2_SIZE),
+                         weight='bold',
+                         )
+        h3Font.configure(size=int(defaultSize * self.H3_SIZE),
+                         slant='italic',
+                         )
+        self.tag_configure(self.H1_TAG,
+                           font=h1Font,
+                           spacing3=defaultSize,
+                           foreground=kwargs['color_chapter'],
+                           justify='center',
+                           spacing1=defaultSize * self.H1_SPACING,
+                           )
+        self.tag_configure(self.H1_NOTES_TAG,
+                           font=h1Font,
+                           spacing3=defaultSize,
+                           foreground=kwargs['color_notes'],
+                           justify='center',
+                           spacing1=defaultSize * self.H1_SPACING,
+                           )
+        self.tag_configure(self.H1_TODO_TAG,
+                           font=h1Font,
+                           spacing3=defaultSize,
+                           foreground=kwargs['color_todo'],
+                           justify='center',
+                           spacing1=defaultSize * self.H1_SPACING,
+                           )
+        self.tag_configure(self.H1_UNUSED_TAG,
+                           font=h1Font,
+                           spacing3=defaultSize,
+                           foreground=kwargs['color_unused'],
+                           justify='center',
+                           spacing1=defaultSize * self.H1_SPACING,
+                           )
+        self.tag_configure(self.H2_TAG,
+                           font=h2Font,
+                           spacing3=defaultSize,
+                           foreground=kwargs['color_chapter'],
+                           justify='center',
+                           spacing1=defaultSize * self.H2_SPACING,
+                           )
+        self.tag_configure(self.H2_NOTES_TAG,
+                           font=h2Font,
+                           spacing3=defaultSize,
+                           foreground=kwargs['color_notes'],
+                           justify='center',
+                           spacing1=defaultSize * self.H2_SPACING,
+                           )
+        self.tag_configure(self.H2_TODO_TAG,
+                           font=h2Font,
+                           spacing3=defaultSize,
+                           foreground=kwargs['color_todo'],
+                           justify='center',
+                           spacing1=defaultSize * self.H2_SPACING,
+                           )
+        self.tag_configure(self.H2_UNUSED_TAG,
+                           font=h2Font,
+                           spacing3=defaultSize,
+                           foreground=kwargs['color_unused'],
+                           justify='center',
+                           spacing1=defaultSize * self.H2_SPACING,
+                           )
+        self.tag_configure(self.H3_NOTES_TAG,
+                           font=h3Font,
+                           spacing3=defaultSize,
+                           foreground=kwargs['color_notes'],
+                           justify='center',
+                           spacing1=defaultSize * self.H3_SPACING,
+                           )
+        self.tag_configure(self.H3_TODO_TAG,
+                           font=h3Font,
+                           spacing3=defaultSize,
+                           foreground=kwargs['color_todo'],
+                           justify='center',
+                           spacing1=defaultSize * self.H3_SPACING,
+                           )
+        self.tag_configure(self.H3_UNUSED_TAG,
+                           font=h3Font,
+                           spacing3=defaultSize,
+                           foreground=kwargs['color_unused'],
+                           justify='center',
+                           spacing1=defaultSize * self.H3_SPACING,
+                           )
+        self.tag_configure(self.NOTES_TAG,
+                           foreground=kwargs['color_notes'],
+                           )
+        self.tag_configure(self.TODO_TAG,
+                           foreground=kwargs['color_todo'],
+                           )
+        self.tag_configure(self.UNUSED_TAG,
+                           foreground=kwargs['color_unused'],
+                           )
 
